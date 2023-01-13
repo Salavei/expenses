137366fcf18cd9dd710f4cf59449b09bebb9e020
Salavei <kano.mori@mail.ru>
2023-01-14 00:08:22 +0300

    Did all the tasks except sorting

--- project/settings.py
+++ project/settings.py
@@ -4,9 +4,9 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = "SEGHT453F2DEFCBRYBVECWSQWDECSVFBREW"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
